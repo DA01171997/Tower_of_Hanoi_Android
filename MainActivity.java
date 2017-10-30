da68aa199d7ed4e0f2ca5d7fa6fbe125e7d333a8
jasonlieu <jasonlieu@users.noreply.github.com>
2017-10-30 10:37:59 -0700

    Add Landscape orientation

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.jason.towers;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
-import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(new GamePanel(this));
     }
 
